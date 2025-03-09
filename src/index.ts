81df0a06e962c5b4d65db84b7f6f1a5ce2c15c8c
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-03-09 19:09:53 +0100

    feat(index.ts): create the initial CLI program
    This creates the initial CLI program with two commands available, help, and version.
    
    closes #7

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export function sayHello() {
-  console.log("Hello");
-}
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+const version: string = "0.0.0";
+
+program
+  .name("freelancer-cli")
+  .version(version)
+  .description(
+    "CLI for freelancers to manage the admin side of their business."
+  );
+
+program.parse(process.argv);
