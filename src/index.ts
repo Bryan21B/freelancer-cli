a468aec9022432e99043358d8cc8e6a92816e7c5
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-03-09 19:32:58 +0100

    chore: fix merge issue

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,7 @@
 import { Command } from "commander";
 
 const program = new Command();
-<<<<<<< HEAD
-const version: string = "0.0.1";
-=======
 const version: string = "0.0.2";
->>>>>>> release/0.0.2
 
 program
   .name("freelancer-cli")
