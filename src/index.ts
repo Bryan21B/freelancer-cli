4a7cd767764e780fba01d76a217be62f155a55ba
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-03-09 19:51:25 +0100

    feat(errors): add error handler
    Add error handler and create custom errors, override default commander errror handler
    
    closes #8

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import { ErrorLevel, handleError } from "./utils/error-handler.js";
+
 import { Command } from "commander";
 
 const program = new Command();
@@ -12,4 +14,21 @@ program
     "CLI for freelancers to manage the admin side of their business."
   );
 
-program.parse(process.argv);
+program.exitOverride((err) => {
+  if (err.code === "commander.missingArgument") {
+    handleError(
+      new Error(`Missing required argument: ${err.message}`),
+      ErrorLevel.ERROR
+    );
+  } else if (err.code === "commander.unknownOption") {
+    handleError(new Error(`Unknown option: ${err.message}`), ErrorLevel.ERROR);
+  } else {
+    handleError(err, ErrorLevel.FATAL);
+  }
+});
+
+try {
+  program.parse(process.argv);
+} catch (error) {
+  handleError(error as Error, ErrorLevel.FATAL);
+}
