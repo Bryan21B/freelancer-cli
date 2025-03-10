6cca62a12c26465f255d89fc79f2678966c97df8
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-03-10 13:11:05 +0100

    feat: add client schema to DB

--- src/db/migrations/20250309215028_burly_true_believers.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `companies_table` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL
-);
---
+++ src/db/schema/clients.ts
@@ -0,0 +1,24 @@
+import { int, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+import { sql } from "drizzle-orm";
+
+export const clientsTable = sqliteTable("companies_table", {
+  clientId: int().primaryKey({ autoIncrement: true }),
+  firstName: text().notNull(),
+  lastName: text().notNull(),
+  companyName: text().notNull(),
+  email: text().unique(),
+  address_street: text(),
+  address_city: text(),
+  address_zip: text(),
+  phoneCountryCode: text(),
+  phoneNumber: text(),
+  createdAt: integer({ mode: "timestamp_ms" })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+  updatedAt: integer({ mode: "timestamp_ms" })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+  archivedAt: integer({ mode: "timestamp_ms" }),
+  isArchived: integer({ mode: "boolean" }).notNull().default(false),
+});
