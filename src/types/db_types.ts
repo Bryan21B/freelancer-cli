f7065566e25eee1f42489d617a22ebe2d48eab87
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-03-11 13:34:50 +0100

    refactor(db): migrate away from Drizzle and to Prisma
    Remove all files linkes to Drizzle, add Prisma, build Prisma schema

--- src/types/db_types.ts
+++
@@ -1,7 +0,0 @@
-import { clients } from "../db/schema/clients";
-import { invoices } from "../db/schema/invoices";
-import { projects } from "../db/schema/projects";
-
-export type DBClient = typeof clients.$inferInsert;
-export type DBProject = typeof projects.$inferInsert;
-export type DBInvoice = typeof invoices.$inferInsert;
