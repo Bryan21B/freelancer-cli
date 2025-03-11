b0f7c588683cfce505f7921c1a208ea22ab14f8f
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-03-11 15:35:24 +0100

    refactor: adjust error handling to take into account Prisma errors

---
+++ src/services/clientService.ts
@@ -0,0 +1,33 @@
+import { Client, NewClient, newClientSchema } from "../types/models";
+
+import { Prisma } from "@prisma/client";
+import { db } from "../prisma/index";
+import { z } from "zod";
+
+export const createClient = async (client: NewClient): Promise<Client> => {
+  try {
+    const validatedClient = newClientSchema.parse(client);
+    return await db.client.create({
+      data: validatedClient,
+    });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      throw new Error(`Invalid client data: ${error.message}`);
+    }
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        throw new Error("A client with this email already exists");
+      }
+    }
+    throw error;
+  }
+};
+
+// export const archiveClientById = async (clientId: Client["id"]) => {
+//   try {
+//     const archivedClient = await db.client.update({
+//       where: { id: clientId },
+//       data: { isArchived: true, archivedAt: new Date() },
+//     });
+//   } catch (error) {}
+// };
