97bdcaa5ccdd358315b83c0ed4c12848d3ed42d0
Bryan Blanchot <Bryan21B@users.noreply.github.com>
2025-03-15 18:59:03 +0100

    feat(client): enhance client command functionality with add and update options
    - Introduce `add` command to create new clients with required fields and optional address details
    - Update `update` command to accept an array for the company name and improve type safety
    - Implement unarchive command for restoring archived clients with user feedback
    - Maintain existing functionality while enhancing command structure and usability

--- src/commands/client.ts
+++ src/commands/client.ts
@@ -1,3 +1,4 @@
+import { Client, NewClient, newClientSchema } from "../types/models.js";
 import {
   FormattedClient,
   formatClientObject,
@@ -5,13 +6,13 @@ import {
 } from "../utils/formatters.js";
 import {
   archiveClientById,
+  createClient,
   getAllClients,
   getClientById,
   unarchiveClientById,
   updateClientById,
 } from "../services/clientService.js";
 
-import { Client } from "../types/models.js";
 import { Command } from "commander";
 import Table from "easy-table";
 import chalk from "chalk";
@@ -61,29 +62,6 @@ export function createClientCommand(): Command {
     });
 
   clientCommand
-    .command("update")
-    // TODO Add validation
-    .description("Update a client by its id")
-    .argument("<id>", "ID of the client to update")
-    .option("--address <addressStreet>", "address of the client")
-    .option("--zip <addressZip>", "zip of the client")
-    .option("--city <addressCity>", "zip of the client")
-    .option("--email <email>", "client's email")
-    .option("--company <company>", "client's company")
-    .option("--phone <phone>", "client's phone number")
-    .option("--first-name <firstName>", "client's first name")
-    .option("--last-name <lastName>", "client's last name")
-    .action(async (rawId: string, options) => {
-      console.log(JSON.stringify(options));
-      const client = await updateClientById(toInt(rawId), options);
-      console.log(
-        chalk.green(`${client.companyName} updated with the details below\n`)
-      );
-
-      console.log(formatClientTable(client));
-    });
-
-  clientCommand
     .command("unarchive")
     .description("Unarchive a client by its ID")
     .argument("<id>", "ID of the client to unarchive")
@@ -92,6 +70,97 @@ export function createClientCommand(): Command {
       console.log(chalk.green(`${client.companyName} is unarchived`));
       console.log(formatClientTable(client));
     });
+
+  clientCommand
+    .command("update")
+    // TODO Add validationa and prompt method
+    .description("Update a client by its id")
+    .argument("<id>", "ID of the client to update")
+    .option("--address <addressStreet>", "address of the client")
+    .option("--zip <addressZip>", "zip of the client")
+    .option("--city <addressCity>", "zip of the client")
+    .option("--email <email>", "client's email")
+    .option("--company [company...]", "client's company")
+    .option("--phone <phone>", "client's phone number")
+    .option("--first-name <firstName>", "client's first name")
+    .option("--last-name <lastName>", "client's last name")
+    .action(
+      async (
+        rawId: string,
+        options: {
+          firstName?: string;
+          lastName?: string;
+          company?: Array<string>;
+          email?: string;
+          address?: string;
+          zip?: string;
+          city?: string;
+          phone?: string;
+        }
+      ) => {
+        const updateData: Partial<NewClient> = {
+          firstName: options.firstName,
+          lastName: options.lastName,
+          companyName: options.company?.join(" "),
+          email: options.email,
+          addressStreet: options.address,
+          addressZip: options.zip,
+          addressCity: options.city,
+          phoneNumber: options.phone,
+        };
+
+        const client = await updateClientById(toInt(rawId), updateData);
+        console.log(
+          chalk.green(`${client.companyName} updated with the details below\n`)
+        );
+
+        console.log(formatClientTable(client));
+      }
+    );
+
+  clientCommand
+    // TODO Add option to use prompt
+    .command("add")
+    .description("Add a new client")
+    .requiredOption("--first-name <firstName>", "client's first name")
+    .requiredOption("--last-name <lastName>", "client's last name")
+    .requiredOption("--email <email>", "client's email")
+    .requiredOption("--company [company...]", "client's company")
+    .option("--address <addressStreet>", "address of the client")
+    .option("--zip <addressZip>", "zip of the client")
+    .option("--city <addressCity>", "zip of the client")
+    .option("--phone <phone>", "client's phone number")
+    .action(
+      async (options: {
+        firstName: string;
+        lastName: string;
+        company: Array<string>;
+        email: string;
+        address?: string;
+        zip?: string;
+        city?: string;
+        phone?: string;
+      }) => {
+        const data: NewClient = {
+          firstName: options.firstName,
+          lastName: options.lastName,
+          companyName: options.company.join(" "),
+          email: options.email,
+          addressStreet: options.address,
+          addressZip: options.zip,
+          addressCity: options.city,
+          phoneNumber: options.phone,
+        };
+
+        const newClient = await createClient(newClientSchema.parse(data));
+        console.log(
+          chalk.green(
+            `${newClient.companyName} created with id ${newClient.id}\n`
+          )
+        );
+        console.log(formatClientTable(newClient));
+      }
+    );
 
   return clientCommand;
 }
